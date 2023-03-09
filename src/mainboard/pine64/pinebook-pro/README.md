f9ca8cd2f39bd5e857908c492e27ca7316784caf
rmsyn <rmsynchls@gmail.com>
2023-03-09 02:18:52 +0000

    mainboard: add pinbook-pro skeleton
    Add a basic skeleton for the pinebook-pro mainboard.
    
    Signed-off-by: rmsyn <rmsynchls@gmail.com>

---
+++ src/mainboard/pine64/pinebook-pro/README.md
@@ -0,0 +1,7 @@
+# Pinebook Pro
+
+Basic firmware for the Pine64 [Pinebook Pro](https://www.pine64.org/pinebook-pro/)
+
+# TODO
+
+Finish the README
---
+++ src/mainboard/pine64/pinebook-pro/src/main.rs
@@ -0,0 +1,24 @@
+#![doc = include_str!("../README.md")]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+unsafe extern "C" fn main() -> usize {
+    0
+}
+
+#[cfg_attr(not(test), panic_handler)]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(_location) = info.location() {
+        // FIXME: uncomment with logging implementation
+        //println!("panic in '{}' line {}", location.file(), location.line(),);
+    } else {
+        // FIXME: uncomment with logging implementation
+        //println!("panic at unknown location");
+    };
+    loop {
+        core::hint::spin_loop();
+    }
+}
